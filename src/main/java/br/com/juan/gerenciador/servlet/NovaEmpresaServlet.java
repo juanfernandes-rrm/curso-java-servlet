853226658128d300c8214eecf6e84cee7cc59ea2
Juan Fernandes <juanfernandesrrm@gmail.com>
2022-06-14 19:33:29 -0300

    aula 02 - Trabalhando com POST e GET

---
+++ src/main/java/br/com/juan/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,26 @@
+package br.com.juan.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nomeEmpresa = request.getParameter("nome");
+		System.out.println("Cadastrando nova empresa"+nomeEmpresa);
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>Empresa "+nomeEmpresa+" cadastrada com sucesso!</body></html>");
+	}
+
+}
